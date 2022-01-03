ba8a84c300995f8d95bc9dd184419ffa592d6603
Alok Soni <37097900+loksoni@users.noreply.github.com>
2022-01-03 11:27:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import praw
+import scraping
+import re
+import praw.exceptions
+from prawcore.exceptions import PrawcoreException
+import time
+import credentials
+
+reddit = praw.Reddit(
+    client_id="hNhzP-xZjm5OBXjbX0u1sg",
+    client_secret="gNtPCvUyqEjFJC_UjQwop4xxdGSuAw",
+    user_agent="<console:AOE2:1.0>",
+    username=credentials.username,
+    password=credentials.password
+)
+
+subreddit = reddit.subreddit("aoe2")
+*tech_keys, = scraping.tech_all
+with open("log_id.txt", "r") as log:
+    prev_id = log.read()
+    print(prev_id)
+    prev_id = prev_id.split("\n")
+    print(prev_id)
+log_id = open("log_id.txt", "a+")
+
+log = open("log.txt", "a+")
+
+
+try:
+    for comment in subreddit.stream.comments():
+        if hasattr(comment, "body") and comment.id not in prev_id:
+            search_word = comment.body.replace("\\", "")
+            word = re.findall(r"\[([A-Za-z0-9\s]+)\]", search_word)
+            if word is not None and len(word)>0:
+                for i in range(0, len(word)):
+                    if word[i].lower() in tech_keys:
+                        print("Match: ", word[i])
+                        comment.reply(word[i] + ": " + scraping.tech_all[word[i]])
+                        print(scraping.tech_all[word[i]])
+                        log.write("Replied to comment: " + comment.body + "\n")
+                        print()
+                log_id.write(comment.id + "\n")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Interrupted by Keyboard")
+except PrawcoreException as e:
+    print("Interrupted by ", e)
+
+log.close()
+log_id.close()
---
+++ test.py
@@ -0,0 +1,25 @@
+'''import re
+str = "test fletching \[fletching\] \[hand cart\]"
+str = str.replace("\\","")
+print(str)
+start_pos = 0
+end_pos = 0
+
+word = re.findall(r"\[([A-Za-z0-9\\\s]+)\]", str)
+
+print(word)
+
+#for i in range(0, len(tech_keys)):
+    #if
+     #   if tech_keys[i] in top_comment.body:
+      #      top_comment.reply(tech_keys[i] +)
+            # print(top_comment.body)
+            # print(scraping.tech_all[tech_keys[i]] + "\nThis action was performed by a bot, I reply to all top level comments that mentions any of the technologies in aoe2 with their corresponding info")'''
+
+
+lis = []
+
+if len(lis)==0:
+    print("empty")
+else:
+    print("not")
